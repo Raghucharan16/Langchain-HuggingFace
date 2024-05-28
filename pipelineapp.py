ffca42e5e5923a177aac4195742b024331123fc9
Narra_Venkata_Raghu_Charan <narravenkataraghucharan03@gmail.com>
2024-05-28 15:52:29 +0530

    Create pipelineapp.py

---
+++ pipelineapp.py
@@ -0,0 +1,13 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer,pipeline
+
+model_id = "microsoft/Phi-3-mini-4k-instruct" #name any model you can want from huggingface hub
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    load_in_4bit=True,
+    #attn_implementation="flash_attention_2", # if you have an ampere GPU 
+)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100, top_k=50, temperature=0.1)
+llm = HuggingFacePipeline(pipeline=pipe)
+llm.invoke("Hugging Face is")
